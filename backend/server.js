dbc24ec73c54bf9966f6d97479046e219a187eb1
ritesh6569 <riteshpatil6569@gmail.com>
2025-04-05 11:22:03 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,21 @@ dotenv.config();
 const app = express();
 
 // Middleware
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://blog2-green-nine.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
 app.use(cors({
   origin: "https://blog2-green-nine.vercel.app",
   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"], // or your frontend domain
